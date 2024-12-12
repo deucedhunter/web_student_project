744a269c9ed5a0edb8177992d49ee0475b76f381
deucedhunter <deucedhunter@gmail.com>
2024-12-12 18:53:14 +0100

    Increase security - make password hidden in environment variable

--- send_email.py
+++ send_email.py
@@ -1,9 +1,9 @@
 import smtplib, ssl
-
+import os
 def send_email(to, message):
     host = 'smtp.gmail.com'
     email_address = 'deucedhunter@gmail.com'
-    password = "nnfn mmyz tdmm gsme"
+    password = os.getenv('PASSWORD')
     port = 465
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
